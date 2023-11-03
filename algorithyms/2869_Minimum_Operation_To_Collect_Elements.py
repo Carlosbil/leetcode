96c6f278daddc6edf4e4811d019457692e4144e3
Carlosbil <carlosbilbao2@gmail.com>
2023-11-03 20:03:36 +0100

    2869 problem solved
    2869 problem solved

---
+++ algorithyms/2869_Minimum_Operation_To_Collect_Elements.py
@@ -0,0 +1,29 @@
+"""
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+Ez matematical aprroach
+# Approach
+<!-- Describe your approach to solving the problem. -->
+If the number is in target, add push, if not, push, pop to the sol array
+# Complexity
+- Time complexity:
+<!-- Add your time complexity here, e.g. $$O(n)$$ -->
+O(n)
+
+- Space complexity:
+<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+O(n)
+# Code
+"""
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        sol = []
+        for x in range(1, n+1):
+            sol.append("Push")
+            if x not in target:
+                sol.append("Pop")
+            if x == target[-1]:
+                break
+        return sol
+
+
